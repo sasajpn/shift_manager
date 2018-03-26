00a5e35f4aa1630f505ad08503b8943aec6dde6d
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-03-27 01:03:46 +0900

    devise用のtableに名前用のカラムを追加

--- db/migrate/20180326154631_devise_create_users.rb
+++ db/migrate/20180326154631_devise_create_users.rb
@@ -3,6 +3,12 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
+
+      t.string :last_name_kana
+      t.string :last_name
+      t.string :first_name_kana
+      t.string :first_name
+
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
