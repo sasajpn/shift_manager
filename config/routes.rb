9cc7eef9c8443992457b13af68a7638b7f03878d
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-03-27 00:54:26 +0900

    devise用のroutingを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :owners
-  devise_for :admins
-  devise_for :users
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
+
+  devise_for :owners, controllers: {
+    sessions:      'owners/sessions',
+    passwords:     'owners/passwords',
+    registrations: 'owners/registrations'
+  }
+  
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
