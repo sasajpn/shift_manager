92beb42327e4a4b70f6d1a208d29345d5b99f202
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-04-21 22:34:45 +0900

    オーナーのテストデータ50個作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,9 @@
 Admin.create(email: 'admin@example.com', password: 'password')
+
+1.upto(50) do |n|
+  Owner.create(
+    email: "owner#{n}@example.com", password: 'password',
+    last_name_kana: 'シハイニン', first_name_kana: 'タロウ',
+    last_name: '支配人', first_name: '太郎'
+  )
+end
