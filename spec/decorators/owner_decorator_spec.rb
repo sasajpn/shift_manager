522edd4b874493c5a433d12e43b7c2674b9f36f5
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-04-21 22:04:33 +0900

    nameのテストが通るように修正

--- spec/decorators/owner_decorator_spec.rb
+++ spec/decorators/owner_decorator_spec.rb
@@ -1,9 +1,9 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe OwnerDecorator do
   describe 'name' do
     let!(:owner) { create(:owner).extend OwnerDecorator }
-    subject { owner.name}
+    subject { owner.name }
     context '姓名が返される' do
       it { is_expected.to eq "#{owner.last_name} #{owner.first_name}"}
     end
