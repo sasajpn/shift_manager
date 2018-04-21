d4d5c45c92580d4a5cf3bd9e6ccb15ba76b7cd50
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-04-21 20:09:33 +0900

    ログイン・ログアウト後のパスを設定

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,26 +1,13 @@
 class Admins::SessionsController < Devise::SessionsController
   layout 'admins/login'
-  # before_action :configure_sign_in_params, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  private
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def after_sign_in_path_for(resource)
+    admins_dashboard_url
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def after_sign_out_path_for(resource)
+    new_admin_session_url
+  end
 end
