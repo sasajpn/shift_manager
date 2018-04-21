e8f4e975477cde689ede598bdbfb4abb18fec15d
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-04-21 21:23:26 +0900

    オーナー登録フォーム作成

--- app/controllers/admins/owners_controller.rb
+++ app/controllers/admins/owners_controller.rb
@@ -6,9 +6,16 @@ class Admins::OwnersController < Admins::ApplicationController
   end
 
   def new
+    @owner = Owner.new
   end
 
   def create
+    @owner = Owner.new(owner_params)
+    if @owner.save
+      redirect_to admins_owners_url
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +25,14 @@ class Admins::OwnersController < Admins::ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def owner_params
+    params.fetch(:owner, {}).permit(
+      :firstname_kana, :lastname_kana, :firstname, :lastname,
+      :email, :password
+    )
   end
 end
