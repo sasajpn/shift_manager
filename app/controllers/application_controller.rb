a28460b2e8190762f26172e06475ecbf2c4624ed
sasajpn <s.kazuki.japan@gmail.com>
2018-04-21 11:30:56 +0900

    Revert "Feature/issue 1"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,3 @@
 class ApplicationController < ActionController::Base
-  include Banken
   protect_from_forgery with: :exception
 end
--- config/application.rb
+++ config/application.rb
@@ -10,25 +10,16 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module ShiftManager
   class Application < Rails::Application
-    config.time_zone = 'Tokyo'
-    config.active_record.default_timezone = :local
-    config.i18n.default_locale = :ja
-    config.generators do |g|
-      g.test_framework :rspec,
-        fixtures: true,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: false,
-        controller_specs: true,
-        request_specs: false
-      g.fixture_replacement :factory_bot, dir: "spec/factories"
-      g.assets     false
-      g.helper     false
-    end
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
   end
 end
