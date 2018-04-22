5a612c3d350c7f92c8612f6bbc3f0f2fb82006ce
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-04-22 09:17:11 +0900

    ユーザのテストデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@ Admin.create(email: 'admin@example.com', password: 'password')
     last_name: '支配人', first_name: '太郎'
   )
 end
+
+1.upto(50) do |n|
+  User.create(
+    email: "user#{n}@example.com", password: 'password',
+    last_name_kana: 'ジュウギョウイン', first_name_kana: 'タロウ',
+    last_name: '従業員', first_name: '太郎'
+  )
+end
