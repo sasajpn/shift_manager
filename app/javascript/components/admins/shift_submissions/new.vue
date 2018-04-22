d0b4340ae8600c11f979ca89ca678368a16b25f8
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-04-23 02:01:23 +0900

    shift_submission_form用のvueテンプレート作成

---
+++ app/javascript/components/admins/shift_submissions/new.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <el-form :model="shiftSubmission" method="post" action="/users" label-width="120px">
+      <el-form-item label="希望日" required>
+        <el-date-picker
+          v-model="shiftSubmission.date"
+          type="date"
+          placeholder="日付を選択してください">
+        </el-date-picker>
+      </el-form-item>
+      <el-form-item label="時刻" required>
+        <el-col :span="11">
+          <el-form-item>
+            <el-time-select
+              placeholder="開始時刻"
+              name="shift_submission[start_time]"
+              v-model="shiftSubmission.startTime"
+              :picker-options="{
+                start: '00:00',
+                step: '00:10',
+                end: '36:00'
+              }">
+            </el-time-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="11">
+          <el-form-item>
+            <el-time-select
+              placeholder="終了時刻"
+              name="shift_submission[end_time]"
+              v-model="shiftSubmission.endTime"
+              :picker-options="{
+                start: '00:00',
+                step: '00:15',
+                end: '36:00',
+                minTime: shiftSubmission.startTime
+              }">
+            </el-time-select>
+          </el-form-item>
+        </el-col>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">登録</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      shiftSubmission: {
+        date: '',
+        startTime: '',
+        endTime: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
