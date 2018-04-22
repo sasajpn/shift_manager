f1483b13198affbb3c963136e0c996a9ba04fcd4
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-04-22 11:43:26 +0900

    従業員の重複チェックを追加

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,5 +2,10 @@ class Member < ApplicationRecord
   belongs_to :team
   belongs_to :user
 
+  validates :user_id, :role,
+    presence: true
+  validates :user_id,
+    uniqueness: { scope: [:team_id], message: 'は既に従業員に登録されています' }
+
   enum role: { part_timer: 0, full_timer: 1, manager: 2 }
 end
