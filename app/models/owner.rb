5db0fd10677c44746cd44dbab0fba44440706c9a
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-04-22 10:38:11 +0900

    未入力とカタカナフォーマットチェックのvalidateを各モデルに追加

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -3,4 +3,9 @@ class Owner < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :teams
+
+  validates :last_name_kana, :first_name_kana, :last_name, :first_name,
+    presence: true
+  validates :last_name_kana, katakana_format: true
+  validates :first_name_kana, katakana_format: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+
+  validates :last_name_kana, :first_name_kana, :last_name, :first_name,
+    presence: true
   validates :last_name_kana, katakana_format: true
   validates :first_name_kana, katakana_format: true
 
