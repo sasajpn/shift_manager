82c6e585fb4ce3394190fdf7c8013e0cf5f29fcc
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-04-25 00:05:41 +0900

    shift_adjustmentのnewページを作成

--- app/controllers/admins/shift_adjustments_controller.rb
+++ app/controllers/admins/shift_adjustments_controller.rb
@@ -1,2 +1,13 @@
-class Admins::ShiftAdjustmentsController < ApplicationController
+class Admins::ShiftAdjustmentsController < Admins::ApplicationController
+  before_action :set_shift_submission, only: [:new]
+
+  def new
+    @shift_adjustment = @shift_submission.build_shift_adjustment
+  end
+
+  private
+
+  def set_shift_submission
+    @shift_submission = ShiftSubmission.find(params[:shift_submission_id])
+  end
 end
--- app/models/shift_submission.rb
+++ app/models/shift_submission.rb
@@ -1,3 +1,5 @@
 class ShiftSubmission < ApplicationRecord
   belongs_to :member
+
+  has_one :shift_adjustment, dependent: :destroy
 end
