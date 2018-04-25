188f131dbd7710d36e5c50fe34ba8ad194397a93
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-04-26 07:30:08 +0900

    シフト希望日

--- app/controllers/admins/shift_adjustments_controller.rb
+++ app/controllers/admins/shift_adjustments_controller.rb
@@ -1,11 +1,26 @@
 class Admins::ShiftAdjustmentsController < Admins::ApplicationController
-  before_action :set_shift_submission, only: [:new]
+  before_action :set_shift_submission, only: [:new, :create]
 
   def new
     @shift_adjustment = @shift_submission.build_shift_adjustment
   end
 
+  def create
+    @shift_adjustment = @shift_submission.build_shift_adjustment(shift_adjustment_params)
+    if @shift_adjustment.save
+      redirect_to admins_shift_submission_url(@shift_submission)
+    else
+      render :new
+    end
+  end
+
   private
+
+  def shift_adjustment_params
+    params.fetch(:shift_adjustment, {}).permit(
+      :start_time, :end_time
+    )
+  end
 
   def set_shift_submission
     @shift_submission = ShiftSubmission.find(params[:shift_submission_id])
