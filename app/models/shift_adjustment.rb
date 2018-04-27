5c82c773811b55722fbe0e18b8a208b5934ca68a
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-04-27 21:14:22 +0900

    シフトが調整された場合、シフト希望を承認、シフトの調整が削除された場合、シフト希望を未承認に更新するように修正

--- app/models/shift_adjustment.rb
+++ app/models/shift_adjustment.rb
@@ -1,3 +1,16 @@
 class ShiftAdjustment < ApplicationRecord
   belongs_to :shift_submission
+
+  after_create :submission_is_approved
+  after_destroy :submission_is_unapproved
+
+  private
+
+  def submission_is_approved
+    shift_submission.is_approved
+  end
+
+  def submission_is_unapproved
+    shift_submission.is_unapproved
+  end
 end
--- app/models/shift_submission.rb
+++ app/models/shift_submission.rb
@@ -1,11 +1,13 @@
 class ShiftSubmission < ApplicationRecord
   belongs_to :member
 
-  has_one :shift_adjustment, dependent: :destroy,
-    before_add :approve_submission,
-    before_remove :cancel_adjustment
+  has_one :shift_adjustment, dependent: :destroy
 
   def is_approved
-    self.update(status: 'approve')
+    self.update(approve: true)
+  end
+
+  def is_unapproved
+    self.update(approve: false)
   end
 end
