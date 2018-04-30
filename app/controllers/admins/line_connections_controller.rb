343219c8e63d15a6d7f5221a351f1206b5650ab4
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-01 02:29:51 +0900

    line_connection用のactionを追加

--- app/controllers/admins/line_connections_controller.rb
+++ app/controllers/admins/line_connections_controller.rb
@@ -1,10 +1,21 @@
-class Admins::LineConnectionsController < Admins::ApplicationController
+class Admins::LineConnectionsController < Devise::SessionsController
+
   def new
+    if admin_signed_in?
+      @line_connection = current_admin.create_line_connection
+      redirect_to "https://access.line.me/dialog/bot/accountLink?linkToken=#{params[:link_token]}&nonce=#{@line_connection.nonce}"
+    else
+      super
+    end
   end
 
   def create
-    @line_connection = current_admin.build_line_connection
-    if @line_connection.save
+    self.resource = warden.authenticate!(auth_options)
+    set_flash_message!(:notice, :signed_in)
+    sign_in(resource_name, resource)
+    yield resource if block_given?
+    if admin_signed_in?
+      @line_connection = current_admin.create_line_connection
       redirect_to "https://access.line.me/dialog/bot/accountLink?linkToken=#{params[:link_token]}&nonce=#{@line_connection.nonce}"
     else
       render :new
