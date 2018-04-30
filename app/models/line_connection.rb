4ffcdfd612ecb95f1b4e38e1337ccb7a2f64f676
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-01 01:03:09 +0900

    line_connectionの作成時にnonceをセットするように修正

--- app/models/line_connection.rb
+++ app/models/line_connection.rb
@@ -1,3 +1,11 @@
 class LineConnection < ApplicationRecord
-  belongs_to :account, :polymorphic => true
+  belongs_to :account, polymorphic: true
+
+  before_create :set_nonce
+
+  private
+
+  def set_nonce
+    self.nonce = SecureRandom.urlsafe_base64
+  end
 end
