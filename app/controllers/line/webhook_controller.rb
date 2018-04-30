9e83a025e726facc383a1607bca0967d52282ca8
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-04-30 21:56:32 +0900

    連携するボタンを押された場合の処理を追加

--- app/controllers/line/webhook_controller.rb
+++ app/controllers/line/webhook_controller.rb
@@ -6,13 +6,29 @@ class Line::WebhookController < ApplicationController
     events = client.parse_events_from(body)
 
     events.each do |event|
-      case event.message['text']
-      when '連携'
-        message = {
-          type: 'text',
-          text: 'OK!'
-        }
-        client.reply_message(event['replyToken'], message)
+      case event
+      when Line::Bot::Event::Postback
+        case event['postback']['data']
+        when 'connecting'
+          message = {
+            type: 'text',
+            text: 'Connecting'
+          }
+          client.reply_message(event['replyToken'], message)
+        end
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
+        end
       end
       "OK"
     end
