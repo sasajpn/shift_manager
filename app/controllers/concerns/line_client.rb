010565a7d5d2442e23cf99f286b7a321da700de8
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-04-30 18:14:26 +0900

    line_client.rbの記述内容修正

--- app/controllers/concerns/line_client.rb
+++ app/controllers/concerns/line_client.rb
@@ -10,14 +10,17 @@ module LineClient
       body = request.body.read
       signature = request.env['HTTP_X_LINE_SIGNATURE']
       unless client.validate_signature(body, signature)
-        error 400 do 'Bad Request' end
+        error 400 do
+          'Bad Request'
+        end
       end
     end
   end
 
   def client
-    @client ||= Line::Bot::Client.new { |config|
+    @client ||= Line::Bot::Client.new do |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    end
   end
 end
