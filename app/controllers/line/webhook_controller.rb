0fb813de243162e4bf4503ff1061926db3a1f7a7
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-04-30 19:27:11 +0900

    botを連携という文字にのみ反応するように修正

--- app/controllers/line/webhook_controller.rb
+++ app/controllers/line/webhook_controller.rb
@@ -1,27 +1,20 @@
 class Line::WebhookController < ApplicationController
-  protect_from_forgery except: [:callback] 
+  protect_from_forgery except: [:callback]
 
   def callback
     body = request.body.read
     events = client.parse_events_from(body)
 
     events.each do |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          client.reply_message(event['replyToken'], message)
-        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-          response = client.get_message_content(event.message['id'])
-          tf = Tempfile.open("content")
-          tf.write(response.body)
-        end
+      case event.message['text']
+      when '連携'
+        message = {
+          type: 'text',
+          text: 'OK!'
+        }
+        client.reply_message(event['replyToken'], message)
       end
+      "OK"
     end
-    "OK"
   end
 end
