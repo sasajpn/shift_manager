f6386bdd2230803343148b0004563e6f6c49526e
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-02 01:27:49 +0900

    line_richmenusコントローラ作成

---
+++ app/controllers/admins/line_richmenus_controller.rb
@@ -0,0 +1,18 @@
+class Admins::LineRichmenusController < Admins::ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @line_richmenu = LineRichmenu.new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
