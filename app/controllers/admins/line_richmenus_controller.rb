8d95f182da84827f412813a7d886dc0d2e009ada
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-02 01:35:11 +0900

    line_richmenuのcreateアクションを作成

--- app/controllers/admins/line_richmenus_controller.rb
+++ app/controllers/admins/line_richmenus_controller.rb
@@ -11,8 +11,22 @@ class Admins::LineRichmenusController < Admins::ApplicationController
   end
 
   def create
+    @line_richmenu = LineRichmenu.new(line_richmenu_params)
+    if @line_richmenu.save
+      redirect_to admins_line_richmenus_url
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def line_richmenu_params
+    params.fetch(:team, {}).permit(
+      :type, :image
+    )
   end
 end
