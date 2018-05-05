fb962263572e520a71fdf9a0b6a0040696380d18
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-06 03:10:11 +0900

    fullcalendarを日本語化

--- app/assets/javascripts/users/teams/show.js
+++ app/assets/javascripts/users/teams/show.js
@@ -1,6 +1,7 @@
 //= require AdminLTE/adminlte.min
 //= require AdminLTE/moment.min
 //= require AdminLTE/fullcalendar.min
+//= require AdminLTE/fullcalendar/ja
 
 $(function() {
   $('#shift_calendar').fullCalendar({
