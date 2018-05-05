8837cf3aab676d1fa8ce4e9bd575320bb40eaade
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-06 03:06:31 +0900

    シフト用のカレンダーを表示する様に修正

--- app/assets/javascripts/users/application.js
+++ app/assets/javascripts/users/application.js
@@ -1,6 +1,2 @@
 //= require jquery
 //= require jquery_ujs
-//= require AdminLTE/adminlte.min
-//= require AdminLTE/app.min
-//= require AdminLTE/fullcalendar.min
-//= require_tree .
---
+++ app/assets/javascripts/users/teams/show.js
@@ -0,0 +1,9 @@
+//= require AdminLTE/adminlte.min
+//= require AdminLTE/moment.min
+//= require AdminLTE/fullcalendar.min
+
+$(function() {
+  $('#shift_calendar').fullCalendar({
+  })
+  console.log('test')
+})
