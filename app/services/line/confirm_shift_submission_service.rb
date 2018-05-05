781f894ee2ef2390196aed50d7dbd7e599bf17e6
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-05 15:57:10 +0900

    開始時間が終了時間を超えていた場合にエラーメッセージを返す様に修正

--- app/services/line/confirm_shift_submission_service.rb
+++ app/services/line/confirm_shift_submission_service.rb
@@ -9,12 +9,35 @@ module Line
     end
 
     def confirm
-      request_confirm_message
+      if start_time.to_time <= end_time.to_time
+        request_confirm_message
+      else
+        request_error_message
+      end
     end
 
     private
 
     attr_reader :reply_token, :start_time, :end_time
+
+    def request_error_message
+      uri = URI.parse("https://api.line.me/v2/bot/message/reply")
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      req = Net::HTTP::Post.new(uri.request_uri)
+      req["Content-Type"] = "application/json"
+      req["Authorization"] = "Bearer #{ENV["LINE_CHANNEL_TOKEN"]}"
+      req.body = {
+        "replyToken": "#{reply_token}",
+        "messages": [
+          {
+            "type": "text",
+            "text": "開始時間が終了時間を超えています\nもう一度シフト希望の提出を行ってください"
+          }
+        ]
+      }.to_json
+      res = http.request(req)
+    end
 
     def request_confirm_message
       uri = URI.parse("https://api.line.me/v2/bot/message/reply")
