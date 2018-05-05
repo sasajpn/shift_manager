13e750b3cd68cf7d80c0b0aac524aca22c28864b
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-06 05:01:03 +0900

    シフトの希望をカレンダーに表示する様に修正

--- app/assets/javascripts/users/teams/show.js
+++ app/assets/javascripts/users/teams/show.js
@@ -4,7 +4,16 @@
 //= require AdminLTE/fullcalendar/ja
 
 $(function() {
+  var team_id = document.getElementById('shift_calendar').dataset.team_id
+  console.log(team_id)
+
   $('#shift_calendar').fullCalendar({
+    timezone: 'Asia/Tokyo',
+    displayEventTime: false,
+    eventSources: [
+      {
+        url: "/api/v1/users/teams/" + team_id
+      }
+    ]
   })
-  console.log('test')
 })
--- app/views/api/v1/users/teams/show.json.jbuilder
+++ app/views/api/v1/users/teams/show.json.jbuilder
@@ -1,4 +1,5 @@
 json.array! @shift_submissions do |shift_submission|
+  json.title "#{shift_submission.start_time}~#{shift_submission.end_time}"
   json.start Chronic.parse("#{shift_submission.submitted_date} #{shift_submission.start_time}")
   json.end Chronic.parse("#{shift_submission.submitted_date} #{shift_submission.end_time}")
   json.editable false
