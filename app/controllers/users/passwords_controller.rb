bb7dfa5ebe4504c51e34bc5004c8369b347f86d6
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-06 23:01:37 +0900

    パスワード再設定メールの文言を修正

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,27 +1,7 @@
 class Users::PasswordsController < Devise::PasswordsController
   layout 'users/register'
 
-  # GET /resource/password/new
-  # def new
-  #   super
-  # end
-
-  # POST /resource/password
-  # def create
-  #   super
-  # end
-
-  # GET /resource/password/edit?reset_password_token=abcdef
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource/password
-  # def update
-  #   super
-  # end
-
-  # protected
+  protected
 
   def after_resetting_password_path_for(resource)
     users_home_index_url
