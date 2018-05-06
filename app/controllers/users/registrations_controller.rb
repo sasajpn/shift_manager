12f1a2191c4ccce7108b8d1a3d282d86ae1e5c59
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-06 17:26:12 +0900

    ユーザ登録ができるように修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,14 +1,13 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   layout 'users/register'
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
-  # protected
+  protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name_kana, :first_name_kana, :last_name, :first_name])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
@@ -16,6 +15,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   def after_sign_up_path_for(resource)
-    
+    users_home_index_url
   end
 end
