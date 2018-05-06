ba144230c656f3448d9a204c7c741dd1213c1e29
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-06 14:21:37 +0900

    memberのroleのデフォルト値をpart_timerに設定

--- db/migrate/20180422020322_create_members.rb
+++ db/migrate/20180422020322_create_members.rb
@@ -3,7 +3,7 @@ class CreateMembers < ActiveRecord::Migration[5.0]
     create_table :members do |t|
       t.references :team, foreign_key: true
       t.references :user, foreign_key: true
-      t.integer :role, null: false
+      t.integer :role, null: false, default: 0
 
       t.timestamps
     end
