a442f4811f55fffc8da133ed54f229864cc3a9b4
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-06 16:54:17 +0900

    usersディレクトリから不要なdevise用のコントローラを削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_one :line_connection, as: :account
-  
+
   has_many :members, dependent: :destroy
   has_many :teams, through: :members
 
