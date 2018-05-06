da81271d7b1a7cdbcf45565af1857571f67403a7
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-06 22:31:13 +0900

    パスワードを更新できるように修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -17,4 +17,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     users_home_index_url
   end
+
+  def after_update_path_for(resource)
+    users_home_index_url
+  end
 end
