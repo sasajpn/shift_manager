2d2337a5cdffbfa6d2651d012ded63c4757d444f
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-06 13:11:32 +0900

    チームに参加するページを作成

--- app/controllers/users/members_controller.rb
+++ app/controllers/users/members_controller.rb
@@ -4,7 +4,15 @@ class Users::MembersController < Users::ApplicationController
   end
 
   def create
+    team = Team.find_by(identifier: params[:identifier])
+    if team
+      current_user.members.create(team_id: team)
+      redirect_to users_teams_url
+    else
+      render :new
+    end
   end
 
   private
+
 end
