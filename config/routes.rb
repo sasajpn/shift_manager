ec66d515304b4d80eee3d89101a15803d258020d
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-06 17:43:32 +0900

    user登録後、確認用のメールが飛ぶように修正

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions:      'users/sessions',
     passwords:     'users/passwords',
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations'
   }
 
   namespace :line do
