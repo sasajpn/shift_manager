d9dcc0a2a2270f9b79e0660780b99c8fca582224
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-12 18:18:07 +0900

    shift_adjustmentのメソッドのテスト作成

--- spec/factories/shift_submissions.rb
+++ spec/factories/shift_submissions.rb
@@ -1,8 +1,15 @@
 FactoryBot.define do
   factory :shift_submission do
-    member nil
-    start_time "2018-04-22 12:40:41"
-    end_time "2018-04-22 12:40:41"
-    status 1
+    member { create(:member) }
+    approve false
+
+    trait :april_first do
+      submitted_date Date.new(2018, 4, 1)
+    end
+
+    trait :eight_am_to_three_pm do
+      start_time '08:00'
+      end_time '15:00'
+    end
   end
 end
