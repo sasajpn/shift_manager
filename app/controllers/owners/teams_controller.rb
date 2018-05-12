1d74f76f927eaadea7d1ef6a448f937e1e1e40a5
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-12 19:33:59 +0900

    owner用のkaminariテーマを作成

--- app/controllers/owners/teams_controller.rb
+++ app/controllers/owners/teams_controller.rb
@@ -1,2 +1,17 @@
-class Owners::TeamsController < ApplicationController
+class Owners::TeamsController < Owners::ApplicationController
+  before_action :set_team, only: [:show]
+  before_action :set_member, only: [:show]
+
+  def index
+    @teams = current_owner.teams.order(created_at: :desc).page(params[:page]).per(15)
+  end
+
+  def show
+  end
+
+  private
+
+  def set_team
+    @team = Team.find(params[:id])
+  end
 end
