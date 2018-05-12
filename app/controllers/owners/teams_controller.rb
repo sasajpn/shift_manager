b7558269c4bdb97896321aa7e7f5b0c2478ddbbe
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-13 05:50:36 +0900

    プリコンパイルの対象を全てのjsとcssにするように修正

--- app/controllers/owners/teams_controller.rb
+++ app/controllers/owners/teams_controller.rb
@@ -1,11 +1,14 @@
 class Owners::TeamsController < Owners::ApplicationController
-  before_action :set_team, only: [:show]
+  before_action :set_team, only: [:show, :edit]
 
   def index
     @teams = current_owner.teams.order(created_at: :desc).page(params[:page]).per(15)
   end
 
   def show
+  end
+
+  def edit
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -45,7 +45,7 @@ Rails.application.routes.draw do
   namespace :owners do
     resources :home, only: [:index]
     resources :owners, only: [:edit, :update]
-    resources :teams, only: [:index, :show], shallow: true do
+    resources :teams, only: [:index, :show, :edit], shallow: true do
       resources :shift_adjustments, only: [:index]
       resources :shift_submissions, except: [:create, :update] do
         resources :shift_adjustments, except: [:index, :create, :update]
