0abc36a5c0c3cbad2542b589a741812678e4ab87
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-13 09:59:07 +0900

    メンバーの役割変更ページを作成

--- app/controllers/owners/members_controller.rb
+++ app/controllers/owners/members_controller.rb
@@ -1,5 +1,5 @@
 class Owners::MembersController < Owners::ApplicationController
-  before_action :set_member, only: [:show]
+  before_action :set_member, only: [:show, :edit, :update, :destroy]
   before_action :set_team, only: [:index, :new, :create]
 
   def index
