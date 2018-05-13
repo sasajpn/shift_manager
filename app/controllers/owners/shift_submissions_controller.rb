13b56fa8a1e5ed72741fda7f788b6933476bafdf
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-13 11:40:02 +0900

    シフト希望の詳細ページを作成

--- app/controllers/owners/shift_submissions_controller.rb
+++ app/controllers/owners/shift_submissions_controller.rb
@@ -1,2 +1,37 @@
-class Owners::ShiftSubmissionsController < ApplicationController
+class Owners::ShiftSubmissionsController < Owners::ApplicationController
+  before_action :set_team, only: [:index, :new, :create]
+  before_action :set_shift_submission, only: [:show]
+
+  def index
+    @shift_submissions = @team.shift_submissions.order(created_at: :desc).page(params[:page]).per(15)
+  end
+
+  def show
+    @member = @shift_submission.member
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def shift_submission_params
+  end
+
+  def set_shift_submission
+    @shift_submission = ShiftSubmission.find(params[:id])
+  end
+
+  def set_team
+    @team = Team.find(params[:team_id])
+  end
 end
