10b082a2f73d82c047e250847dc0b8988aa097d4
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-13 11:14:52 +0900

    未承認の従業員を承認・否認できるように修正

--- app/controllers/owners/members/unapprovals_controller.rb
+++ app/controllers/owners/members/unapprovals_controller.rb
@@ -1,11 +1,41 @@
 class Owners::Members::UnapprovalsController < Owners::ApplicationController
+  before_action :set_member, only: [:show, :edit, :update, :destroy]
   before_action :set_team, only: [:index]
 
   def index
     @members = @team.members.unapprovals.order(created_at: :desc).page(params[:page]).per(15)
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @member.update(member_params)
+      redirect_to owners_team_members_url(@member.team)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @member.destroy
+    redirect_to owners_team_url(@member.team)
+  end
+
   private
+
+  def member_params
+    params.fetch(:member, {}).permit(
+      :role, :approve
+    )
+  end
+
+  def set_member
+    @member = Member.find(params[:id])
+  end
 
   def set_team
     @team = Team.find(params[:team_id])
