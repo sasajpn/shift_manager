43293cea57c8721bcceb7928e18e448c6af6a5b8
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-05-20 00:06:12 +0900

    シフト表を日付送りできる様に修正

--- app/javascript/components/owners/shift_tables/ShiftTableHeader.vue
+++ app/javascript/components/owners/shift_tables/ShiftTableHeader.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="fc-toolbar fc-header-toolbar">
     <div class="fc-left">
-      <h2>2018年 5月</h2>
+      <h2>{{ formattedDate }}</h2>
     </div>
     <div class="fc-right">
       <el-button-group>
-        <el-button type="primary">今日</el-button>
-        <el-button type="primary" icon="el-icon-arrow-left"></el-button>
-        <el-button type="primary" icon="el-icon-arrow-right"></el-button>
+        <el-button type="primary" v-if="isToday()" disabled>今日</el-button>
+        <el-button type="primary" v-else v-on:click="returnToday">今日</el-button>
+        <el-button type="primary" icon="el-icon-arrow-left" v-on:click="subtractDate"></el-button>
+        <el-button type="primary" icon="el-icon-arrow-right" v-on:click="addDate"></el-button>
       </el-button-group>
     </div>
     <div class="fc-center">
@@ -17,5 +18,36 @@
 </template>
 
 <script>
+  export default {
+    data() {
+      return {
+        currentDate: this.moment(),
+        formattedDate: ''
+      }
+    },
+    methods: {
+      moment() {
+        return moment()
+      },
+      addDate() {
+        this.currentDate.add(1, 'days')
+        this.formattedDate = this.currentDate.format('YYYY年MM月DD日')
+      },
+      subtractDate() {
+        this.currentDate.subtract(1, 'days')
+        this.formattedDate = this.currentDate.format('YYYY年MM月DD日')
+      },
+      returnToday() {
+        this.currentDate = this.moment()
+        this.formattedDate = this.currentDate.format('YYYY年MM月DD日')
+      },
+      isToday() {
+        return this.formattedDate === this.moment().format('YYYY年MM月DD日')
+      }
+    },
+    created() {
+      this.formattedDate = this.currentDate.format('YYYY年MM月DD日')
+    }
+  }
 
 </script>
--- app/javascript/packs/owners/application.js
+++ app/javascript/packs/owners/application.js
@@ -7,11 +7,14 @@ import * as _ from 'lodash'
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/ja'
 
+import moment from 'moment'
+
 const library = {
   Vue: Vue,
   '_': _,
   ElementUI: ElementUI,
-  locale: locale
+  locale: locale,
+  moment: moment
 }
 
 _.each(library, function(plugin, namespace) {
