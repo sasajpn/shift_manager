45009f836539830cdb73dcf6207c68d7d802c682
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-06-03 22:22:28 +0900

    シフト調整編集ページを作成

--- app/controllers/users/part_timers/shift_adjustments_controller.rb
+++ app/controllers/users/part_timers/shift_adjustments_controller.rb
@@ -1,4 +1,5 @@
 class Users::PartTimers::ShiftAdjustmentsController < Users::ApplicationController
+  before_action :set_shift_adjustment, only: [:edit]
   before_action :set_shift_submission, only: [:new]
   before_action :set_team, only: [:index]
   before_action :set_current_member, only: [:index]
@@ -9,7 +10,15 @@ class Users::PartTimers::ShiftAdjustmentsController < Users::ApplicationControll
   def new
   end
 
+  def edit
+    @shift_submission = @shift_adjustment.shift_submission
+  end
+
   private
+
+  def set_shift_adjustment
+    @shift_adjustment = Shift::Adjustment.find(params[:id])
+  end
 
   def set_shift_submission
     @shift_submission = ShiftSubmission.find(params[:shift_submission_id])
