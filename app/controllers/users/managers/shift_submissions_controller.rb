33ed3f23083e1026cd62d27fb39f66f0abfc1d88
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-06-05 18:53:15 +0900

    全ロールのshift_submissionページを作成

--- app/controllers/users/managers/shift_submissions_controller.rb
+++ app/controllers/users/managers/shift_submissions_controller.rb
@@ -1,24 +1,2 @@
-class Users::Managers::ShiftSubmissionsController < Users::ApplicationController
-  before_action :set_shift_submission, only: [:show]
-  before_action :set_team, only: [:index, :show]
-  before_action :set_current_member, only: [:index, :show]
-
-  def index
-  end
-
-  def show
-    @member = @shift_submission.member
-    @shift_adjustment = @shift_submission.shift_adjustment
-  end
-
-  private
-
-  def set_shift_submission
-    @shift_submission = ShiftSubmission.find(params[:id])
-  end
-
-  def set_team
-    super
-    @team ||= @shift_submission.team
-  end
+class Users::Managers::ShiftSubmissionsController < Users::ShiftSubmissionsController
 end
