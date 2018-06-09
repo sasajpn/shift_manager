77f98fda369b7b12c7d22bf9006dedb9c9571b97
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-06-09 12:10:41 +0900

    teamのshowにuserのそのteamにおける全シフトを表示するように修正

--- app/controllers/api/v1/users/teams/calendars_controller.rb
+++ app/controllers/api/v1/users/teams/calendars_controller.rb
@@ -1,19 +1,12 @@
 class Api::V1::Users::Teams::CalendarsController < Api::V1::Users::ApplicationController
   before_action :set_team, only: [:index]
-  before_action :set_member, only: [:index]
+  before_action :set_current_member, only: [:index]
 
   def index
-    @shift_submissions = @member.shift_submissions.unapprovals
-    @shift_adjustments = @member.shift_adjustments
+    @shift_submissions = @current_member.shift_submissions.unapprovals
+    @shift_adjustments = @current_member.shift_adjustments
+    @shift_registrations = @current_member.shift_registrations
   end
 
   private
-
-  def set_team
-    @team = Team.find(params[:team_id])
-  end
-
-  def set_member
-    @member = @team.members.find_by(user_id: current_user.id)
-  end
 end
