b8654f88053aa42eb79f04391922d1198486177f
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-06-10 14:16:48 +0900

    シフト調整詳細画面を削除

--- app/controllers/owners/shift_adjustments_controller.rb
+++ app/controllers/owners/shift_adjustments_controller.rb
@@ -1,14 +1,10 @@
 class Owners::ShiftAdjustmentsController < Owners::ApplicationController
-  before_action :set_shift_adjustment, only: [:show, :edit, :destroy]
+  before_action :set_shift_adjustment, only: [:edit, :destroy]
   before_action :set_shift_submission, only: [:new]
   before_action :set_team, only: [:index]
 
   def index
     @shift_adjustments = @team.shift_adjustments.page(params[:page]).per(15)
-  end
-
-  def show
-    @member = @shift_adjustment.member
   end
 
   def new
