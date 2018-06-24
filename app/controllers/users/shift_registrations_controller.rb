80453f4fffaf5e2e958793c43bbf88edffb538e1
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-06-24 10:51:09 +0900

    shift_registrationのloyaltyでのエラーを修正

--- app/controllers/users/shift_registrations_controller.rb
+++ app/controllers/users/shift_registrations_controller.rb
@@ -1,25 +1,23 @@
 class Users::ShiftRegistrationsController < Users::ApplicationController
   before_action :set_shift_registration, only: [:edit]
-  before_action :set_member, only: [:new]
+  before_action :set_member, only: [:new, :edit]
+  before_action -> { authorize! @member }
 
   def new
     @team = @member.team
-    authorize! @team
   end
 
   def edit
-    @member = @shift_registration.member
     @team = @member.team
-    authorize! @team
   end
 
   private
 
   def set_shift_registration
-    @shift_registration = Shift::Registration.find(params[:id])
+    @shift_registration = Shift::Registration.find_by(id: params[:id])
   end
 
   def set_member
-    @member = Member.find(params[:member_id])
+    @member = Member.find_by(id: params[:member_id]) || @shift_registration.member
   end
 end
