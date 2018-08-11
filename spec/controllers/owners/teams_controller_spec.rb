aa6ff2d2b4048689b2c954f4a97c0b5c7b2f8ee1
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-08-12 02:21:41 +0900

    owner用のteams_controllerのテストを全アクションで作成

--- spec/controllers/owners/teams_controller_spec.rb
+++ spec/controllers/owners/teams_controller_spec.rb
@@ -6,16 +6,97 @@ RSpec.describe Owners::TeamsController, type: :controller do
   let!(:other_team) { create(:team) }
 
   describe 'GET #show' do
-    context 'ログイン済みで、チームがログイン済みのオーナーのものである場合' do
-      it 'showテンプレートがrenderされる' do
-        get :show, params: { id: team.id }
-        expect(response).to render_template :show
+    context 'ログインしていない場合' do
+      before do
+        sign_out subject.current_owner
+      end
+      it 'ログイン画面にリダイレクトする' do
+        get :index
+        expect(response).to redirect_to new_owner_session_url
       end
     end
-    context 'ログイン済みで、チームがログイン済みのオーナーのものでない場合' do
-      it 'ホーム画面にリダイレクトする' do
-        get :show, params: { id: other_team.id }
-        expect(response).to redirect_to owners_home_index_url
+    context 'ログインしている場合' do
+      it 'indexテンプレートがレンダリングされる' do
+        get :index
+        expect(response).to render_template :index
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    context 'ログインしていない場合' do
+      before do
+        sign_out subject.current_owner
+      end
+      it 'ログイン画面にリダイレクトする' do
+        get :show, params: { id: team.id }
+        expect(response).to redirect_to new_owner_session_url
+      end
+    end
+    context 'ログインしている場合' do
+      context 'チームがログイン済みのオーナーのものである場合' do
+        it 'showテンプレートがレンダリングされる' do
+          get :show, params: { id: team.id }
+          expect(response).to render_template :show
+        end
+      end
+      context 'チームがログイン済みのオーナーのものでない場合' do
+        it 'オーナー用のホーム画面にリダイレクトする' do
+          get :show, params: { id: other_team.id }
+          expect(response).to redirect_to owners_home_index_url
+        end
+      end
+    end
+  end
+
+  describe 'GET #edit' do
+    context 'ログインしていない場合' do
+      before do
+        sign_out subject.current_owner
+      end
+      it 'ログイン画面にリダイレクトする' do
+        get :edit, params: { id: team.id }
+        expect(response).to redirect_to new_owner_session_url
+      end
+    end
+    context 'ログインしている場合' do
+      context 'チームがログイン済みのオーナーのものである場合' do
+        it 'showテンプレートがレンダリングされる' do
+          get :edit, params: { id: team.id }
+          expect(response).to render_template :edit
+        end
+      end
+      context 'チームがログイン済みのオーナーのものでない場合' do
+        it 'オーナー用のホーム画面にリダイレクトする' do
+          get :edit, params: { id: other_team.id }
+          expect(response).to redirect_to owners_home_index_url
+        end
+      end
+    end
+  end
+
+  describe 'PATCH #update_identifier' do
+    context 'ログインしていない場合' do
+      before do
+        sign_out subject.current_owner
+      end
+      it 'ログイン画面にリダイレクトする' do
+        patch :update_identifier, params: { id: team.id }
+        expect(response).to redirect_to new_owner_session_url
+      end
+    end
+    context 'ログインしている場合' do
+      context 'チームがログイン済みのオーナーのものである場合' do
+        it 'データが更新される' do
+          patch :update_identifier, params: { id: team.id }
+          expect(response).to redirect_to owners_team_url(team)
+        end
+      end
+      context 'チームがログイン済みのオーナーのものでない場合' do
+        it 'オーナー用のホーム画面にリダイレクトする' do
+          patch :update_identifier, params: { id: other_team.id }
+          expect(response).to redirect_to owners_home_index_url
+        end
       end
     end
   end
