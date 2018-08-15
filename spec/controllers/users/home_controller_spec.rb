002914bae586a613c47758be9a4acbdfc66f6005
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-08-15 18:19:26 +0900

    homeコントローラのテストを作成

--- spec/controllers/users/home_controller_spec.rb
+++ spec/controllers/users/home_controller_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Users::HomeController, type: :controller do
+  login_user
 
+  describe 'GET #index' do
+    context 'ログインしていない場合' do
+      before do
+        sign_out subject.current_user
+      end
+      it 'ログイン画面にリダイレクトする' do
+        get :index
+        expect(response).to redirect_to new_user_session_url
+      end
+    end
+    context 'ログインしている場合' do
+      it 'indexテンプレートがレンダリングされる' do
+        get :index
+        expect(response).to render_template :index
+      end
+    end
+  end
 end
--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -15,8 +15,10 @@ module ControllerMacros
   end
 
   def login_user
-    controller.stub(:authenticate_user!).and_return true
-    @request.env["devise.mapping"] = Devise.mappings[:user]
-    sign_in user
+    before(:each) do
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      user = FactoryBot.create(:user)
+      sign_in user
+    end
   end
 end
