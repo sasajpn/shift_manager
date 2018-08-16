ec0a7a07fce153b9c0bcf2e78f2b6b8c9895e235
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-08-17 08:16:35 +0900

    managers/shift_submissionsのテストを作成

--- app/controllers/users/managers/shift_submissions_controller.rb
+++ app/controllers/users/managers/shift_submissions_controller.rb
@@ -1,6 +1,26 @@
-class Users::Managers::ShiftSubmissionsController < Users::ShiftSubmissionsController
-  before_action -> { authorize! @current_member }, only: [:show]
+class Users::Managers::ShiftSubmissionsController < Users::ApplicationController
+  before_action :set_shift_submission, only: [:show]
+  before_action :set_team, only: [:index, :show]
+
+  include Users::Managers::AccessControl
+  before_action :check_valid_permisson, only: [:index, :show]
+
+  def index
+  end
 
   def show
+    @memer = @shift_submission.member
+    @shift_adjustment = @shift_submission.shift_adjustment
+  end
+
+  private
+
+  def set_shift_submission
+    @shift_submission = ShiftSubmission.find(params[:id])
+  end
+
+  def set_team
+    super
+    @team ||= @shift_submission.team
   end
 end
