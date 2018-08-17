b8b435f9b31acb8048bc8ab9df36e2f5a9ad31a7
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-08-17 18:20:43 +0900

    managers/teamsコントローラのテストを作成

--- app/controllers/users/managers/teams_controller.rb
+++ app/controllers/users/managers/teams_controller.rb
@@ -1,7 +1,9 @@
 class Users::Managers::TeamsController < Users::ApplicationController
   before_action :set_team, only: [:update_identifier]
-  before_action :set_current_member, only: [:update_identifier]
-  before_action -> { authorize! @current_member }
+  before_action :set_member, only: [:update_identifier]
+
+  include Users::Managers::AccessControl
+  before_action :check_valid_permisson, only: [:update_identifier]
 
   def update_identifier
     if @team.update(identifier: SecureRandom.hex(5))
@@ -9,5 +11,15 @@ class Users::Managers::TeamsController < Users::ApplicationController
     else
       render :show
     end
+  end
+
+  private
+
+  def set_team
+    @team = Team.find_by(id: params[:id])
+  end
+
+  def set_member
+    @member = @team.members.find_by(user_id: current_user.id)
   end
 end
--- app/loyalties/users/managers/shift_submissions_loyalty.rb
+++
@@ -1,9 +0,0 @@
-class Users::Managers::ShiftSubmissionsLoyalty < Users::ApplicationLoyalty
-  def index?
-    member&.manager?
-  end
-
-  def show?
-    member&.manager?
-  end
-end
