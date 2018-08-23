5179bc811c4409f681f1a2ac9aa07e72784a271e
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-08-23 15:43:07 +0900

    shift_coordinatoras/shift_registrationsコントローラのテスト作成

---
+++ app/controllers/users/shift_coordinators/shift_registrations_controller.rb
@@ -0,0 +1,29 @@
+class Users::ShiftCoordinators::ShiftRegistrationsController < Users::ApplicationController
+  before_action :set_shift_registration, only: [:edit]
+  before_action :set_member, only: [:new, :edit]
+  before_action :set_team, only: [:new, :edit]
+
+  include Users::ShiftCoordinators::AccessControl
+  before_action :check_valid_permisson, only: [:new, :edit]
+
+  def new
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_shift_registration
+    @shift_registration = Shift::Registration.find_by(id: params[:id])
+  end
+
+  def set_member
+    super
+    @member ||= @shift_registration.member
+  end
+
+  def set_team
+    @team = @member&.team || @shift_registration&.team
+  end
+end
---
+++ spec/controllers/users/shift_coordinators/shift_registrations_controller_spec.rb
@@ -0,0 +1,211 @@
+require 'rails_helper'
+
+RSpec.describe Users::ShiftCoordinators::ShiftRegistrationsController, type: :controller do
+  login_user
+
+  let!(:team) { create(:team) }
+
+  let!(:member) { create(:member, team: team) }
+  let!(:part_timer) { create(:member, :part_timer, team: team) }
+  let!(:full_timer) { create(:member, :full_timer, team: team) }
+  let!(:manager) { create(:member, :manager, team: team) }
+
+  let!(:shift_registration) { create(:shift_registration, :tomorrow, :eight_am_to_three_pm, member: member) }
+  let!(:part_timer_shift_registration) { create(:shift_registration, :tomorrow, :eight_am_to_three_pm, member: part_timer) }
+  let!(:full_timer_shift_registration) { create(:shift_registration, :tomorrow, :eight_am_to_three_pm, member: full_timer) }
+  let!(:manager_shift_registration) { create(:shift_registration, :tomorrow, :eight_am_to_three_pm, member: manager) }
+
+  describe 'GET #new' do
+    context 'ログインしていない場合' do
+      before do
+        sign_out subject.current_user
+      end
+      it 'ログイン画面にリダイレクトする' do
+        get :new, params: { member_id: member.id }
+        expect(response).to redirect_to new_user_session_url
+      end
+    end
+    context 'ログインしている場合' do
+      context '自チームのメンバーである場合' do
+        context 'シフト調整権限がある場合' do
+          let!(:my_member) { create(:member, :shift_coordinator, role: role, team: team, user: subject.current_user) }
+          context 'マネージャーの場合' do
+            let!(:role) { 'manager' }
+            context 'アクセス先のメンバーがマネージャーである場合' do
+              it 'newテンプレートがレンダリングされる' do
+                get :new, params: { member_id: manager.id }
+                expect(response).to render_template :new
+              end
+            end
+            context 'アクセス先のメンバーが正社員である場合' do
+              it 'newテンプレートがレンダリングされる' do
+                get :new, params: { member_id: full_timer.id }
+                expect(response).to render_template :new
+              end
+            end
+            context 'アクセス先のメンバーがパートタイマーである場合' do
+              it 'newテンプレートがレンダリングされる' do
+                get :new, params: { member_id: part_timer.id }
+                expect(response).to render_template :new
+              end
+            end
+          end
+          context '正社員の場合' do
+            let!(:role) { 'full_timer' }
+            context 'アクセス先のメンバーがマネージャーである場合' do
+              it 'newテンプレートがレンダリングされる' do
+                get :new, params: { member_id: manager.id }
+                expect(response).to redirect_to users_home_index_url
+              end
+            end
+            context 'アクセス先のメンバーが正社員である場合' do
+              it 'newテンプレートがレンダリングされる' do
+                get :new, params: { member_id: full_timer.id }
+                expect(response).to render_template :new
+              end
+            end
+            context 'アクセス先のメンバーがパートタイマーである場合' do
+              it 'newテンプレートがレンダリングされる' do
+                get :new, params: { member_id: part_timer.id }
+                expect(response).to render_template :new
+              end
+            end
+          end
+          context 'パートタイマーの場合' do
+            let!(:role) { 'part_timer' }
+            context 'アクセス先のメンバーがマネージャーである場合' do
+              it 'newテンプレートがレンダリングされる' do
+                get :new, params: { member_id: manager.id }
+                expect(response).to redirect_to users_home_index_url
+              end
+            end
+            context 'アクセス先のメンバーが正社員である場合' do
+              it 'newテンプレートがレンダリングされる' do
+                get :new, params: { member_id: full_timer.id }
+                expect(response).to redirect_to users_home_index_url
+              end
+            end
+            context 'アクセス先のメンバーがパートタイマーである場合' do
+              it 'newテンプレートがレンダリングされる' do
+                get :new, params: { member_id: part_timer.id }
+                expect(response).to render_template :new
+              end
+            end
+          end
+        end
+        context 'シフト調整権限がない場合' do
+          let!(:my_member) { create(:member, team: team, user: subject.current_user) }
+          it 'ユーザー用のホーム画面にリダイレクトする' do
+            get :new, params: { member_id: member.id }
+            expect(response).to redirect_to users_home_index_url
+          end
+        end
+      end
+      context '他チームのメンバーである場合' do
+        let!(:other_member) { create(:member, :shift_coordinator, user: subject.current_user) }
+        it 'ユーザー用のホーム画面にリダイレクトする' do
+          get :new, params: { member_id: member.id }
+          expect(response).to redirect_to users_home_index_url
+        end
+      end
+    end
+  end
+
+
+  describe 'GET #edit' do
+    context 'ログインしていない場合' do
+      before do
+        sign_out subject.current_user
+      end
+      it 'ログイン画面にリダイレクトする' do
+        get :edit, params: { id: shift_registration.id }
+        expect(response).to redirect_to new_user_session_url
+      end
+    end
+    context 'ログインしている場合' do
+      context '自チームのメンバーである場合' do
+        context 'シフト調整権限がある場合' do
+          let!(:my_member) { create(:member, :shift_coordinator, role: role, team: team, user: subject.current_user) }
+          context 'マネージャーの場合' do
+            let!(:role) { 'manager' }
+            context 'アクセス先のシフト登録がマネージャーのものである場合' do
+              it 'editテンプレートがレンダリングされる' do
+                get :edit, params: { id: manager_shift_registration.id }
+                expect(response).to render_template :edit
+              end
+            end
+            context 'アクセス先のシフト登録が正社員のものである場合' do
+              it 'editテンプレートがレンダリングされる' do
+                get :edit, params: { id: full_timer_shift_registration.id }
+                expect(response).to render_template :edit
+              end
+            end
+            context 'アクセス先のシフト登録がパートタイマーのものである場合' do
+              it 'editテンプレートがレンダリングされる' do
+                get :edit, params: { id: part_timer_shift_registration.id }
+                expect(response).to render_template :edit
+              end
+            end
+          end
+          context '正社員の場合' do
+            let!(:role) { 'full_timer' }
+            context 'アクセス先のシフト登録がマネージャーのものである場合' do
+              it 'editテンプレートがレンダリングされる' do
+                get :edit, params: { id: manager_shift_registration.id }
+                expect(response).to redirect_to users_home_index_url
+              end
+            end
+            context 'アクセス先のシフト登録が正社員のものである場合' do
+              it 'editテンプレートがレンダリングされる' do
+                get :edit, params: { id: full_timer_shift_registration.id }
+                expect(response).to render_template :edit
+              end
+            end
+            context 'アクセス先のシフト登録がパートタイマーのものである場合' do
+              it 'editテンプレートがレンダリングされる' do
+                get :edit, params: { id: part_timer_shift_registration.id }
+                expect(response).to render_template :edit
+              end
+            end
+          end
+          context 'パートタイマーの場合' do
+            let!(:role) { 'part_timer' }
+            context 'アクセス先のシフト登録がマネージャーのものである場合' do
+              it 'editテンプレートがレンダリングされる' do
+                get :edit, params: { id: manager_shift_registration.id }
+                expect(response).to redirect_to users_home_index_url
+              end
+            end
+            context 'アクセス先のシフト登録が正社員のものである場合' do
+              it 'editテンプレートがレンダリングされる' do
+                get :edit, params: { id: full_timer_shift_registration.id }
+                expect(response).to redirect_to users_home_index_url
+              end
+            end
+            context 'アクセス先のシフト登録がパートタイマーのものである場合' do
+              it 'editテンプレートがレンダリングされる' do
+                get :edit, params: { id: part_timer_shift_registration.id }
+                expect(response).to render_template :edit
+              end
+            end
+          end
+        end
+        context 'シフト調整権限がない場合' do
+          let!(:my_member) { create(:member, team: team, user: subject.current_user) }
+          it 'ユーザー用のホーム画面にリダイレクトする' do
+            get :edit, params: { id: shift_registration.id }
+            expect(response).to redirect_to users_home_index_url
+          end
+        end
+      end
+      context '他チームのメンバーである場合' do
+        let!(:other_member) { create(:member, :shift_coordinator, user: subject.current_user) }
+        it 'ユーザー用のホーム画面にリダイレクトする' do
+          get :edit, params: { id: shift_registration.id }
+          expect(response).to redirect_to users_home_index_url
+        end
+      end
+    end
+  end
+
+end
