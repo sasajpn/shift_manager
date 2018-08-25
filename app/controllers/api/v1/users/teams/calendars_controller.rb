1d525dd625b0fa80821772ee2edd8968bf2267fd
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-08-25 21:37:40 +0900

    api/teams/calendarsコントローラのテストを作成

--- app/controllers/api/v1/users/teams/calendars_controller.rb
+++ app/controllers/api/v1/users/teams/calendars_controller.rb
@@ -1,12 +1,24 @@
 class Api::V1::Users::Teams::CalendarsController < Api::V1::Users::ApplicationController
   before_action :set_team, only: [:index]
-  before_action :set_current_member, only: [:index]
+  before_action :set_member, only: [:index]
+
+  include Api::Users::AccessControl
+  before_action :check_valid_permisson, only: [:index]
 
   def index
-    @shift_submissions = @current_member.shift_submissions.unapprovals
-    @shift_adjustments = @current_member.shift_adjustments
-    @shift_registrations = @current_member.shift_registrations
+    @shift_submissions = @member.shift_submissions.unapprovals
+    @shift_adjustments = @member.shift_adjustments
+    @shift_registrations = @member.shift_registrations
+    render 'index', formats: [:json], handlers: [:jbuilder]
   end
 
   private
+
+  def have_valid_permission?
+    @member ? true : false
+  end
+
+  def set_member
+    @member = @team.member(current_user)
+  end
 end
