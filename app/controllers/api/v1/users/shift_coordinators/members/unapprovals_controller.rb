a2f0c3d63a4c2f712b72283fa14bf7cb013f6583
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-09-02 13:21:36 +0900

    apiのunapprovalsコントローラのテストを作成

--- app/controllers/api/v1/users/shift_coordinators/members/unapprovals_controller.rb
+++ app/controllers/api/v1/users/shift_coordinators/members/unapprovals_controller.rb
@@ -2,17 +2,24 @@ class Api::V1::Users::ShiftCoordinators::Members::UnapprovalsController < Api::V
   before_action :set_member, only: [:update]
   before_action :set_team, only: [:update]
 
+  include Api::Users::AccessControl
+  before_action :check_valid_permisson
+
   def update
     if @member.update(member_params)
       @success_message = 'メンバーの参加を承認しました。'
       render 'api/v1/shared/success', formats: [:json], handlers: [:jbuilder]
     else
-      @error_messages = @shift_adjustment.errors.full_messages
+      @error_messages = @member.errors.full_messages
       render "api/v1/shared/error_messages", formats: [:json], handlers: [:jbuilder]
     end
   end
 
   private
+
+  def have_valid_permission?
+    @team.manager?(current_user) && !@member.approve
+  end
 
   def member_params
     params.fetch(:member, {}).permit(
