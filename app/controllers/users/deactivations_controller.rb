9d99fad23c73c82227726e02a95b05603f3a4fcb
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-09-09 11:47:05 +0900

    不要なメソッドを削除

--- app/controllers/users/deactivations_controller.rb
+++ app/controllers/users/deactivations_controller.rb
@@ -5,7 +5,7 @@ class Users::DeactivationsController < Users::ApplicationController
 
   def destroy
     if current_user.destroy
-      redirect_to new_user_session_url
+      redirect_to new_user_session_url, notice: '退会しました。ご利用ありがとうございました。'
     else
       redirect_to users_home_index_url, alert: '未来に調整されたシフト、もしくは登録されたシフトが残っているため退会できません。'
     end
