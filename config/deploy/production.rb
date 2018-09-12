f9cfbe72657842128003fb25eb14949988e2cfad
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-09-13 01:07:26 +0900

    ssh_optionsを修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,7 +2,8 @@ role :app, %w{kazuki@133.130.125.29}
 role :web, %w{kazuki@133.130.125.29}
 role :db,  %w{kazuki@133.130.125.29}
 set :ssh_options, {
-  port: 22,
+  port: '22',
   keys: File.expand_path('~/.ssh/shifpon_server'),
-  auth_methods: %w(publickey)
+  forward_agent: true,
+  auth_methods: %w{publickey}
 }
