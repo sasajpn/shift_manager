6b09f6058b60f5c35cdcf6e21ec27fd715545404
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-09-13 00:06:13 +0900

    capistranoの設定を追加

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,29 +1,25 @@
-rails_root = File.expand_path('../../', __FILE__)
+current_path = '/var/www/shift_manager/current'
+shared_path = '/var/www/shift_manager/shared'
 
-ENV['BUNDLE_GEMFILE'] = rails_root + "/Gemfile"
-
-working_directory rails_root
+timeout 30
 
 worker_processes 2
-timeout 15
+working_directory "#{current_path}"
 
-stderr_path File.expand_path('../../log/unicorn_stderr.log', __FILE__)
-stdout_path File.expand_path('../../log/unicorn_stdout.log', __FILE__)
+listen "#{shared_path}/tmp/sockets/unicorn.sock"
+pid "#{shared_path}/tmp/pids/unicorn.pid"
 
-listen File.expand_path('../../tmp/sockets/unicorn.sock', __FILE__)
-pid File.expand_path('../../tmp/pids/unicorn.pid', __FILE__)
+stderr_path "#{shared_path}/log/unicorn.stderr.log"
+stdout_path "#{shared_path}/log/unicorn.stdout.log"
 
 preload_app true
 
 before_fork do |server, worker|
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.connection.disconnect!
-
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
   old_pid = "#{server.config[:pid]}.oldbin"
   if old_pid != server.pid
     begin
-      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
-      Process.kill(sig, File.read(old_pid).to_i)
+      Process.kill "QUIT", File.read(old_pid).to_i
     rescue Errno::ENOENT, Errno::ESRCH
     end
   end
