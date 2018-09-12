5c50fed4a3076f2915df1504ff2e940fd3a9ebd1
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-09-13 00:47:36 +0900

    deploy/production.rbを修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,5 +3,5 @@ role :web, %w{kazuki@133.130.125.29}
 role :db,  %w{kazuki@133.130.125.29}
 set :ssh_options, {
   port: 22,
-  keys: File.expand_path('~/.ssh/github_rsa'),
+  keys: File.expand_path('~/.ssh/shifpon_server'),
 }
