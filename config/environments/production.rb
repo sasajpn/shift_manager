755996db293a7a4d9e3c632d935555c661390e1d
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-09-16 03:23:16 +0900

    deviseのsenderを変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.default_url_options = { host: 'shifpon.com' }
+  config.action_mailer.default_url_options = { host: 'http://shifpon.com' }
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
