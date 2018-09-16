7da96db31a5540da7d1f98b44524351778ec5c94
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-09-16 14:58:39 +0900

    確認メールの再送ページを作成

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -1,8 +1,9 @@
 class Users::ConfirmationsController < Devise::ConfirmationsController
+  layout 'users/register'
   protected
 
   def after_resending_confirmation_instructions_path_for(resource_name)
-    users_home_index_url
+    new_user_session_url
   end
 
   def after_confirmation_path_for(resource_name, resource)
