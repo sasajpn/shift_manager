0533a139c730acddd2fd577367549091218dcd49
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-09-17 02:16:24 +0900

    トップページにflashを表示するように修正

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -3,10 +3,6 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
   protected
 
   def after_resending_confirmation_instructions_path_for(resource_name)
-    new_user_session_url
-  end
-
-  def after_confirmation_path_for(resource_name, resource)
-    users_home_index_url
+    root_url
   end
 end
