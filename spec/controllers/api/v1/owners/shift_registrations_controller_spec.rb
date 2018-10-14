397a1c4830ad280ae3f79e1b3dda0cd77ca457a8
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2018-10-14 23:56:09 +0900

    controllerのテストが通るように修正

--- spec/controllers/api/v1/owners/shift_registrations_controller_spec.rb
+++ spec/controllers/api/v1/owners/shift_registrations_controller_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe Api::V1::Owners::ShiftRegistrationsController, type: :controller do
   login_owner
 
-  let!(:team) { create(:team, owner: subject.current_owner) }
-  let!(:other_team) { create(:team) }
+  let!(:team) {create(:team, owner: subject.current_owner)}
+  let!(:other_team) {create(:team)}
 
-  let!(:member) { create(:member, team: team) }
-  let!(:other_member) { create(:member) }
+  let!(:member) {create(:member, team: team)}
+  let!(:other_member) {create(:member)}
 
-  let!(:shift_registration) { create(:shift_registration, member: member) }
-  let!(:other_shift_registration) { create(:shift_registration) }
+  let!(:shift_registration) {create(:shift_registration, member: member)}
+  let!(:other_shift_registration) {create(:shift_registration)}
 
   describe 'GET #show' do
     context 'ログインしていない場合' do
@@ -18,31 +18,21 @@ RSpec.describe Api::V1::Owners::ShiftRegistrationsController, type: :controller 
         sign_out subject.current_owner
       end
       it 'ログイン画面にリダイレクトする' do
-        get :show, params: { id: shift_registration.id }
+        get :show, params: {id: shift_registration.id}
         expect(response).to redirect_to new_owner_session_url
       end
     end
     context 'ログインしている場合' do
       context 'シフト登録がログイン済みのオーナーのチームのものである場合' do
-        context 'チームの有効期限が切れていない場合' do
-          it '200が返ってくる' do
-            get :show, params: { id: shift_registration.id }
-            expect(response).to be_success
-            expect(response.status).to eq 200
-          end
-        end
-        context 'チームの有効期限が切れている場合' do
-          it '404が返ってくる' do
-            team.update(active_until: Time.current.yesterday)
-            get :show, params: { id: shift_registration.id }
-            expect(response).not_to be_success
-            expect(response.status).to eq 404
-          end
+        it '200が返ってくる' do
+          get :show, params: {id: shift_registration.id}
+          expect(response).to be_success
+          expect(response.status).to eq 200
         end
       end
       context 'シフト登録がログイン済みのオーナーのチームのものでない場合' do
         it '404が返ってくる' do
-          get :show, params: { id: other_shift_registration.id }
+          get :show, params: {id: other_shift_registration.id}
           expect(response).not_to be_success
           expect(response.status).to eq 404
         end
@@ -52,38 +42,38 @@ RSpec.describe Api::V1::Owners::ShiftRegistrationsController, type: :controller 
 
   describe 'POST #create' do
     let!(:create_attributes) do
-      { registered_date: Date.today.next_month, start_time: '10:00', end_time: '12:00' }
+      {registered_date: Date.today.next_month, start_time: '10:00', end_time: '12:00'}
     end
     context 'ログインしていない場合' do
       before do
         sign_out subject.current_owner
       end
       it 'ログイン画面にリダイレクトする' do
-        post :create, params: { member_id: member.id }
+        post :create, params: {member_id: member.id}
         expect(response).to redirect_to new_owner_session_url
       end
     end
     context 'ログインしている場合' do
       context 'シフト登録がログイン済みのオーナーのチームのものである場合' do
         it 'シフト登録が作成される' do
-          expect{
-            post :create, params: { member_id: member.id, shift_registration: create_attributes }
+          expect {
+            post :create, params: {member_id: member.id, shift_registration: create_attributes}
           }.to change(Shift::Registration, :count).by(1)
         end
         it '200が返ってくる' do
-          post :create, params: { member_id: member.id, shift_registration: create_attributes }
+          post :create, params: {member_id: member.id, shift_registration: create_attributes}
           expect(response).to be_success
           expect(response.status).to eq 200
         end
       end
       context 'シフト登録がログイン済みのオーナーのチームのものでない場合' do
         it 'シフト登録が作成されない' do
-          expect{
-            post :create, params: { member_id: other_member.id, shift_registration: create_attributes }
+          expect {
+            post :create, params: {member_id: other_member.id, shift_registration: create_attributes}
           }.to change(Shift::Registration, :count).by(0)
         end
         it '404が返ってくる' do
-          post :create, params: { member_id: other_member.id, shift_registration: create_attributes }
+          post :create, params: {member_id: other_member.id, shift_registration: create_attributes}
           expect(response).not_to be_success
           expect(response.status).to eq 404
         end
@@ -93,38 +83,38 @@ RSpec.describe Api::V1::Owners::ShiftRegistrationsController, type: :controller 
 
   describe 'PATCH #update' do
     let!(:update_attributes) do
-      { start_time: '10:00' }
+      {start_time: '10:00'}
     end
     context 'ログインしていない場合' do
       before do
         sign_out subject.current_owner
       end
       it 'ログイン画面にリダイレクトする' do
-        patch :update, params: { id: shift_registration.id }
+        patch :update, params: {id: shift_registration.id}
         expect(response).to redirect_to new_owner_session_url
       end
     end
     context 'ログインしている場合' do
       context 'シフト登録がログイン済みのオーナーのものである場合' do
         it 'シフト登録が更新される' do
-          patch :update, params: { id: shift_registration.id, shift_registration: update_attributes }
+          patch :update, params: {id: shift_registration.id, shift_registration: update_attributes}
           shift_registration.reload
           expect(shift_registration.start_time).to eq '10:00'
         end
         it '200が返ってくる' do
-          patch :update, params: { id: shift_registration.id }
+          patch :update, params: {id: shift_registration.id}
           expect(response).to be_success
           expect(response.status).to eq 200
         end
       end
       context 'シフト登録がログイン済みのオーナーのチームのものでない場合' do
         it 'シフト登録が更新されない' do
-          patch :update, params: { id: other_shift_registration.id, shift_registration: update_attributes }
+          patch :update, params: {id: other_shift_registration.id, shift_registration: update_attributes}
           shift_registration.reload
           expect(shift_registration.start_time).to eq '08:00'
         end
         it '404が返ってくる' do
-          patch :update, params: { id: other_shift_registration.id }
+          patch :update, params: {id: other_shift_registration.id}
           expect(response).not_to be_success
           expect(response.status).to eq 404
         end
--- spec/controllers/owners/members/unapprovals_controller_spec.rb
+++ spec/controllers/owners/members/unapprovals_controller_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe Owners::Members::UnapprovalsController, type: :controller do
   login_owner
 
-  let!(:team) { create(:team, owner: subject.current_owner) }
-  let!(:other_team) { create(:team) }
+  let!(:team) {create(:team, owner: subject.current_owner)}
+  let!(:other_team) {create(:team)}
 
-  let!(:member) { create(:member, team: team) }
-  let!(:other_member) { create(:member) }
+  let!(:member) {create(:member, team: team)}
+  let!(:other_member) {create(:member)}
 
   describe 'GET #index' do
     context 'ログインしていない場合' do
@@ -15,29 +15,20 @@ RSpec.describe Owners::Members::UnapprovalsController, type: :controller do
         sign_out subject.current_owner
       end
       it 'ログイン画面にリダイレクトする' do
-        get :index, params: { team_id: team.id }
+        get :index, params: {team_id: team.id}
         expect(response).to redirect_to new_owner_session_url
       end
     end
     context 'ログインしている場合' do
       context 'チームがログイン済みのオーナーのものである場合' do
-        context 'チームの有効期限がきれていない場合' do
-          it 'indexテンプレートがレンダリングされる' do
-            get :index, params: { team_id: team.id }
-            expect(response).to render_template :index
-          end
-        end
-        context 'チームの有効期限が切れている場合' do
-          it 'オーナー用のホーム画面にリダイレクトする' do
-            team.update(active_until: Time.current.yesterday)
-            get :index, params: { team_id: team.id }
-            expect(response).to redirect_to owners_home_index_url
-          end
+        it 'indexテンプレートがレンダリングされる' do
+          get :index, params: {team_id: team.id}
+          expect(response).to render_template :index
         end
       end
       context 'チームがログイン済みのオーナーのものでない場合' do
         it 'オーナー用のホーム画面にリダイレクトする' do
-          get :index, params: { team_id: other_team.id }
+          get :index, params: {team_id: other_team.id}
           expect(response).to redirect_to owners_home_index_url
         end
       end
@@ -50,29 +41,20 @@ RSpec.describe Owners::Members::UnapprovalsController, type: :controller do
         sign_out subject.current_owner
       end
       it 'ログイン画面にリダイレクトする' do
-        get :show, params: { id: member.id }
+        get :show, params: {id: member.id}
         expect(response).to redirect_to new_owner_session_url
       end
     end
     context 'ログインしている場合' do
       context 'メンバーがログイン済みのオーナーのものである場合' do
-        context 'チームの有効期限が切れていない場合' do
-          it 'showテンプレートがレンダリングされる' do
-            get :show, params: { id: member.id }
-            expect(response).to render_template :show
-          end
-        end
-        context 'チームの有効期限が切れている場合' do
-          it 'オーナー用のホーム画面にリダイレクトする' do
-            team.update(active_until: Time.current.yesterday)
-            get :show, params: { id: member.id }
-            expect(response).to redirect_to owners_home_index_url
-          end
+        it 'showテンプレートがレンダリングされる' do
+          get :show, params: {id: member.id}
+          expect(response).to render_template :show
         end
       end
       context 'メンバーがログイン済みのオーナーのものでない場合' do
         it 'オーナー用のホーム画面にリダイレクトする' do
-          get :show, params: { id: other_member.id }
+          get :show, params: {id: other_member.id}
           expect(response).to redirect_to owners_home_index_url
         end
       end
@@ -85,29 +67,20 @@ RSpec.describe Owners::Members::UnapprovalsController, type: :controller do
         sign_out subject.current_owner
       end
       it 'ログイン画面にリダイレクトする' do
-        get :edit, params: { id: member.id }
+        get :edit, params: {id: member.id}
         expect(response).to redirect_to new_owner_session_url
       end
     end
     context 'ログインしている場合' do
       context 'メンバーがログイン済みのオーナーのものである場合' do
-        context 'チームの有効期限が切れていない場合' do
-          it 'editテンプレートがレンダリングされる' do
-            get :edit, params: { id: member.id }
-            expect(response).to render_template :edit
-          end
-        end
-        context 'チームの有効期限が切れている場合' do
-          it 'オーナー用のホーム画面にリダイレクトする' do
-            team.update(active_until: Time.current.yesterday)
-            get :edit, params: { id: member.id }
-            expect(response).to redirect_to owners_home_index_url
-          end
+        it 'editテンプレートがレンダリングされる' do
+          get :edit, params: {id: member.id}
+          expect(response).to render_template :edit
         end
       end
       context 'メンバーがログイン済みのオーナーのものでない場合' do
         it 'オーナー用のホーム画面にリダイレクトする' do
-          get :edit, params: { id: other_member.id }
+          get :edit, params: {id: other_member.id}
           expect(response).to redirect_to owners_home_index_url
         end
       end
@@ -120,45 +93,30 @@ RSpec.describe Owners::Members::UnapprovalsController, type: :controller do
         sign_out subject.current_owner
       end
       it 'ログイン画面にリダイレクトする' do
-        delete :destroy, params: { id: member.id }
+        delete :destroy, params: {id: member.id}
         expect(response).to redirect_to new_owner_session_url
       end
     end
     context 'ログインしている場合' do
       context 'メンバーがログイン済みのオーナーのものである場合' do
-        context 'チームの有効期限が切れていない場合' do
-          it 'メンバーが削除される' do
-            expect {
-              delete :destroy, params: { id: member.id }
-            }.to change(Member, :count).by(-1)
-          end
-          it 'チームのshowページにリダイレクトされる' do
-            delete :destroy, params: { id: member.id }
-            expect(response).to redirect_to owners_team_url(member.team)
-          end
+        it 'メンバーが削除される' do
+          expect {
+            delete :destroy, params: {id: member.id}
+          }.to change(Member, :count).by(-1)
         end
-        context 'チームの有効期限が切れている場合' do
-          it 'メンバーが削除されない' do
-            team.update(active_until: Time.current.yesterday)
-            expect {
-              delete :destroy, params: { id: member.id }
-            }.to change(Member, :count).by(0)
-          end
-          it 'オーナー用のホーム画面にリダイレクトする' do
-            team.update(active_until: Time.current.yesterday)
-            delete :destroy, params: { id: member.id }
-            expect(response).to redirect_to owners_home_index_url
-          end
+        it 'チームのshowページにリダイレクトされる' do
+          delete :destroy, params: {id: member.id}
+          expect(response).to redirect_to owners_team_url(member.team)
         end
       end
       context 'メンバーがログイン済みのオーナーのものでない場合' do
         it 'メンバーが削除されない' do
           expect {
-            delete :destroy, params: { id: other_member.id }
+            delete :destroy, params: {id: other_member.id}
           }.to change(Member, :count).by(0)
         end
         it 'オーナー用のホーム画面にリダイレクトする' do
-          delete :destroy, params: { id: other_member.id }
+          delete :destroy, params: {id: other_member.id}
           expect(response).to redirect_to owners_home_index_url
         end
       end
